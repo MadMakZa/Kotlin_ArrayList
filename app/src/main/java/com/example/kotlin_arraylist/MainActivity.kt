2fd0681b999fe110e95ca34272cc2783ccc51033
MadMakZa <immakza@gmail.com>
2020-12-25 19:09:12 +0300

    Initial commit

---
+++ app/src/main/java/com/example/kotlin_arraylist/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.kotlin_arraylist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var nameList = ArrayList<String>()
+        nameList.add("Max")
+        nameList.add("Misha")
+        nameList.add("Danya")
+        nameList.add("Serega")
+        nameList.add("Dima")
+        nameList.add("Ilia")
+
+
+    }
+}
