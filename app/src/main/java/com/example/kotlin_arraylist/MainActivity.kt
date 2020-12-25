de79cf0bb7fadaddcae0d497ed300d5d667dc928
MadMakZa <immakza@gmail.com>
2020-12-25 19:13:08 +0300

    shablon adapter

--- app/src/main/java/com/example/kotlin_arraylist/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_arraylist/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.kotlin_arraylist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +20,9 @@ class MainActivity : AppCompatActivity() {
         nameList.add("Dima")
         nameList.add("Ilia")
 
+        //адаптер (кэп)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, nameList)
+        listView.adapter = adapter
 
     }
 }
